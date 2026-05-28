07e9f20ab11e2d1e77be40300e6a966a7d557a2d
David Cavalcante <david.beloc@outlook.com>
2026-05-28 15:26:23 -0300

    Update and rename codigoappdesktop.py to dave_ticket.py

--- dave_ticket.py
+++ dave_ticket.py
@@ -1,6 +1,20 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv() 
+
+conexao = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    database=os.getenv("DB_NAME"),
+    port=os.getenv("DB_PORT"),
+    sslmode="require"
+)
 
 
 def carregar_vendas():
